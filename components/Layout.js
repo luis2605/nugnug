0891fae10a8637e6e82db0470ad8b5265694c6ab
Luis Rondon <luisrondon2605@yahoo.com>
2023-09-09 22:59:30 +0200

    scroll

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
-// fonts
 import { Sora } from "@next/font/google";
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import TopLeftImg from "../components/TopLeftImg";
 
 // font settings
 const sora = Sora({
@@ -8,21 +10,21 @@ const sora = Sora({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
 });
 
-// components
-import Nav from "../components/Nav";
-import Header from "../components/Header";
-import TopLeftImg from "../components/TopLeftImg";
-
 const Layout = ({ children }) => {
   return (
     <div
       className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
+      style={{ overflowY: "scroll", height: "100vh" }}
     >
       <TopLeftImg />
-      <Nav />
-      <Header />
+      <Nav style={{ position: "sticky", top: 0, zIndex: 1000 }} />
+      <Header style={{ position: "sticky", top: 0, zIndex: 1000 }} />
       <div
-        className="overflow-y-scroll h-screen" // Add overflow-y-scroll and a max height
+        className="z-0" // Lower z-index for the children
+        style={{
+          position: "relative", // This allows the z-index to take effect
+          paddingTop: "3rem", // Adjust this value as needed to avoid overlap
+        }}
       >
         {children}
       </div>
